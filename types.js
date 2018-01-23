33096b1f9619d01ba0abbc25b1823be14ee5c53f
arronHealy <G00346883@gmit.ie>
2018-01-23 13:43:27 +0000

    test types

---
+++ types.js
@@ -0,0 +1,7 @@
+var isDone = false;
+console.log("isDone is " + isDone);
+var decimal = 6;
+var hex = 0xf00d;
+var binary = 10;
+var octal = 484;
+console.log("Decimal is " + decimal + "\nHex is " + hex + "\nbinary is " + binary + "\noctal is " + octal);
---
+++ types.ts
@@ -0,0 +1,10 @@
+
+let isDone: boolean = false;
+console.log("isDone is " + isDone);
+
+let decimal: number = 6;
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
+let octal: number = 0o744;
+
+console.log("Decimal is " + decimal + "\nHex is " + hex + "\nbinary is " + binary + "\noctal is " + octal);
